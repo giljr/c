77aa56af82b5f2fd94c8e0976a726ac125b8a676
J3 <giljr.2009@gmail.com>
2020-06-13 18:21:42 -0400

    Project2 - Halting the system w/ while:)
    Project2 - Halting the system w/ while:)

--- _01_if_while_dowhile_for/Solution1/Proj2/app2.cpp
+++ _01_if_while_dowhile_for/Solution1/Proj2/app2.cpp
@@ -1,11 +1,14 @@
 /*
   Using while statement
 
+  Description:
+  You enter any number and the system tells you if it is even or odd number.
+  To halt the process type 0 or any negative number;)
+  This is implemented by using while loop. Simple like that:)
+
   Iteration statements
   The iteration statements repeatedly execute a statement.
-
   while ( expression ) statement	(1)
-
   author: j3
   date: jun, 12/2020
 */
