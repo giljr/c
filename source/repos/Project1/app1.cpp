92da372425b86e2cc6a3145171b84a81f0653be5
J3 <giljr.2009@gmail.com>
2020-06-14 20:08:56 -0400

    Update app1.cpp

--- source/repos/Project1/app1.cpp
+++ source/repos/Project1/app1.cpp
@@ -10,7 +10,8 @@
   if ( expression ) statement_1
   if ( expression ) statement_1 else statement_2
 
-  author: j3
+  Author: Borin (https://br.linkedin.com/in/borinvini) 
+  Edited by j3
   date: jun, 12/2020
 */
 #include <stdio.h>
