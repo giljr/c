13a6de9d2a379c0a3bb0e0963d2f9e8590e519d9
J3 <giljr.2009@gmail.com>
2020-06-22 16:47:33 -0400

    Project 26 - How to declare floats - Calc Circumference and Area of a Circular Table
    Project 26 - How to declare floats - Calc Circumference and Area of a Circular Table

--- DEVC/EX26/main.c
+++ DEVC/EX26/main.c
@@ -39,7 +39,7 @@ int main(void) {
 	
  float radius = 0.0f; 				// The radius of the table
  float diameter = 0.0f; 			// The diameter of the table
- float circumference = 0.0f; 		// The circumference of the table
+ float circumference = 0.0f; 			// The circumference of the table
  float area = 0.0f; 				// The area of the table
  float Pi = 3.14159265f;
  
@@ -47,7 +47,7 @@ int main(void) {
  scanf("%f", &diameter); 			// Read the diameter from the keyboard
  
  radius = diameter/2.0f; 			// Calculate the radius
- circumference = 2.0f*Pi*radius; 	// Calculate the circumference
+ circumference = 2.0f*Pi*radius; 		// Calculate the circumference
  area = Pi*radius*radius; 			// Calculate the area
  
  printf("\nThe circumference is %.2f", circumference);
