45e37023556f90141bf53325a910415d697f7bc2
J3 <giljr.2009@gmail.com>
2020-07-02 07:59:32 -0400

    Project 01 - Studying C Types (char, int, float & double)
    Project 01 - Studying C Types (char, int, float & double)

--- DEVC/EX01/Project.c
+++ DEVC/EX01/Project.c
@@ -5,8 +5,8 @@
    	****************************
    	Type 	Descrition 	  Bits
    	****************************
-   	char 	character 		8 
-   	int	 	integer			16			
+   	char 	character 	8 
+   	int	 	integer	16			
    	float 	floating point  32
    	double 	floating point  64 
    	****************************
