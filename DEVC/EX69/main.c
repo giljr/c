d8632e8abc8468554dbb6966539ed6affd002f07
J3 <giljr.2009@gmail.com>
2021-06-12 06:28:33 -0400

    Update Project 69 - more debug :+1:

--- DEVC/EX69/main.c
+++ DEVC/EX69/main.c
@@ -10,7 +10,7 @@
  How to resolve  this error: 
 
 
- â€˜forâ€™ loop initial declarations are only allowed in C99 or C11 mode) ?
+ ‘for’ loop initial declarations are only allowed in C99 or C11 mode) ?
 
 
  Configure Dev C++ to use the ansi iso c99 standard (recommended to avoid further compatibility issues with the exercises in the book)
@@ -24,29 +24,32 @@
  ***************************** 
   Output:
   
- words = 4225568, words[0] = 4210688, words[1] = 4210693, words[2] = 4210697
- multiple indirection example
- zero
- one
- two
- ppc = 4225568
+	words = 4225568, words[0] = 4210688, words[1] = 4210693, words[2] = 4210697
+	multiple indirection example
+	zero
+	one
+	two
+	
+	ppc = 4225568
+	
+	Looping
+	ppc = 4225568
+	z-> pc = 4210689
+	e-> pc = 4210690
+	r-> pc = 4210691
+	o-> pc = 4210692
+	ppc = 4225576
+	o-> pc = 4210694
+	n-> pc = 4210695
+	e-> pc = 4210696
+	ppc = 4225584
+	t-> pc = 4210698
+	w-> pc = 4210699
+	o-> pc = 4210700
+	Loop End
+	
+	ppc = 4225584
 
- Looping
- z pc = 4210689
- e pc = 4210690
- r pc = 4210691
- o pc = 4210692
-
- o pc = 4210694
- n pc = 4210695
- e pc = 4210696
-
- t pc = 4210698
- w pc = 4210699
- o pc = 4210700
-
- Loop End
- ppc = 4225584
  ***************************** 
 
   	Author: Huw Collingbourne
@@ -64,14 +67,14 @@
 char *words[LENGTH];		        // some strings
 
 int main(int argc, char **argv) {
-	char *pc;			// a pointer to a character
+	char *pc;			            // a pointer to a character
 	
-	char **ppc;			// a pointer to a pointer to a character
-                                        // The asterisk(*), which is sometimes called the  
-					// deferencing operator or the indirection operator, 
-					// gives me access to the data it is stored at that address
+	char **ppc;			            // a pointer to a pointer to a character
+                                    // The asterisk(*), which is sometimes called the  
+						            // deferencing operator or the indirection operator, 
+						            // gives me access to the data it is stored at that address
 	                                // Remember: (&) gives me reference to an address
-                                        //           (*) gives me access    to the data  									
+                                    //           (*) gives me access    to the data  									
 	/* initialize our string array  */
 	words[0] = "zero";
 	words[1] = "one";
@@ -92,21 +95,22 @@ int main(int argc, char **argv) {
 	*
 	*/
 	ppc = words;
-	printf("ppc = %d\n\n", ppc);	// initialize the pointer to a pointer to a character
+	printf("\nppc = %d\n\n", ppc);	// initialize the pointer to a pointer to a character
 	printf("Looping\n");
 	for ( i = 0; i < LENGTH; i++) {	// loop over each string
 		ppc = words + i;
+		printf("ppc = %d\n", ppc);
 		pc = *ppc;		
-		while (*pc != 0) {	// process each character in a string
-		printf("%c ", *pc);	// print out each character of the string individually
-		pc += 1;
-		printf("pc = %d\n", pc);			
-	        }
+		while (*pc != 0) {			// process each character in a string
+			printf("%c", *pc);		// print out each character of the string individually
+			pc += 1;
+			printf("-> pc = %d\n", pc);			
+		}
 		
 		//printf("\n");
 	}
 	printf("Loop End\n");
 	
-        printf("ppc = %d\n", ppc);
+    printf("\nppc = %d\n", ppc);
 	return 0;
 }
